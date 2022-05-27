8750dc747a5e3aedf8100c87066b713898ea16e3
602897 <kevin.franks@va.gov>
2022-05-27 16:45:19 -0400

    Added manage user

---
+++ app/user/views.py
@@ -0,0 +1,30 @@
+"""
+Views for User Api
+"""
+from rest_framework import authentication, generics, permisions
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+
+from user.serializers import AuthTokenSerializer, UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system"""
+    serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """create a new auth token for user"""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetieveUpdateAPIView):
+    """Manage Auth user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permisions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
